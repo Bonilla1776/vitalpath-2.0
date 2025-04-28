f43709a70d368955d60cd208da9f78a5ee71b950
JohnEric-Creator <masterbonilla@gmail.com>
2025-04-27 21:17:10 -0700

    Add /healthz endpoint for Azure Healthcheck

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,11 @@ app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(conversation.router, prefix="/conversation", tags=["conversation"])
 
+
 @app.get("/")
 def read_root():
     return {"message": "VitalPath 2.0 Backend is Running"}
+
+@app.get("/healthz")
+def health_check():
+    return {"status": "ok"}    
