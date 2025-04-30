62dbfae8499dabe3c6c4c4787ec45bed61d3c0b1
JohnEric-Creator <masterbonilla@gmail.com>
2025-04-29 17:41:11 -0700

    WIP: Save local changes before pulling main

--- backend/app/tests/test_healthz.py
+++ backend/app/tests/test_healthz.py
@@ -1,8 +1,8 @@
 import requests
 
 def test_healthz_endpoint():
-    url = "https://api.vitalpathinnovations.com/healthz"
+    url = "https://vitalpath-backend.gentlepebble-9bae58f5.westus2.azurecontainerapps.io/healthz"
     response = requests.get(url)
     assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+
 
